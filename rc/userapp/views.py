652e45e96fdeb1267f41d25f719e3d1332ef3193
manasi-g <manasiganu02@gmail.com>
2020-09-13 13:37:51 +0530

    m

--- rc/userapp/views.py
+++ rc/userapp/views.py
@@ -6,7 +6,8 @@ from django.urls import reverse
 import random
 from datetime import datetime, timedelta
 import re
-from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+from django.views.decorators.cache import cache_control
 
 
 def signup(request):
@@ -91,100 +92,89 @@ def logout(request):
                   {'message': 'You logged out Successfully.', 'score': user1.current_score})
 
 
+@never_cache
 def quiz(request):
-    username = None
-    if request.user.is_authenticated:
-        username = request.user.username
-    user = User.objects.get(username=username)
-    if request.method == "GET":
-        try:
-            
-            user_to_get = UserProfile.objects.get(user=user)
-            present_time = datetime.now()
-            present_time = (present_time.hour * 3600) + (present_time.minute * 60) + (present_time.second)
-            end_time = (user_to_get.predicted_logout_time.hour * 3600) + (user_to_get.predicted_logout_time.minute * 60) + (user_to_get.predicted_logout_time.second)
-            timer = end_time - present_time
-            if timer <= 0:
-                return redirect('logout')
-            if user_to_get.no_question_solved >= 10:
-                user_to_get.user_logout = 1
-                user_to_get.logout_time = datetime.now()
-                user_to_get.save(update_fields=["user_logout", 'logout_time'])
-                auth.logout(request)
-                return render(request, 'userapp/SignIn.html',
+    try:
+        if request.user.is_authenticated:
+            username = request.user.username
+            user = User.objects.get(username=username)
+        if request.method == "GET":
+            try:
+                user_to_get = UserProfile.objects.get(user=user)
+                present_time = datetime.now()
+                present_time = (present_time.hour * 3600) + (present_time.minute * 60) + (present_time.second)
+                end_time = (user_to_get.predicted_logout_time.hour * 3600) + (user_to_get.predicted_logout_time.minute * 60) + (user_to_get.predicted_logout_time.second)
+                timer = end_time - present_time
+                if timer <= 0:
+                    return redirect('logout')
+                if user_to_get.no_question_solved >= 10:
+                    user_to_get.user_logout = 1
+                    user_to_get.logout_time = datetime.now()
+                    user_to_get.save(update_fields=["user_logout", 'logout_time'])
+                    auth.logout(request)
+                    return render(request, 'userapp/SignIn.html',
                               {'message': 'You logged out Successfully.', 'score': user_to_get.current_score})
-            if len(user_to_get.question_attempted.split(" ")) >= 2:
-                response_to_get = Response.objects.filter(user=user.id).last()
-                if (response_to_get.attempt_1 == -1 & response_to_get.attempt_2 == -1) | (user_to_get.decision == 1):
-                    ques = Question.objects.get(id=user_to_get.current_ques_id)
-                    current_score_to_show = user_to_get.current_score
-                    sr_no = user_to_get.no_question_solved + 1
-                    present_time = datetime.now()
-                    present_time = (present_time.hour * 3600) + (present_time.minute * 60) + (present_time.second)
-                    end_time = (user_to_get.predicted_logout_time.hour * 3600) + (
+                if len(user_to_get.question_attempted.split(" ")) >= 2:
+                    response_to_get = Response.objects.filter(user=user.id).last()
+                    if (response_to_get.attempt_1 == -1 & response_to_get.attempt_2 == -1) | (user_to_get.decision == 1):
+                        ques = Question.objects.get(id=user_to_get.current_ques_id)
+                        current_score_to_show = user_to_get.current_score
+                        sr_no = user_to_get.no_question_solved + 1
+                        present_time = datetime.now()
+                        present_time = (present_time.hour * 3600) + (present_time.minute * 60) + (present_time.second)
+                        end_time = (user_to_get.predicted_logout_time.hour * 3600) + (
                                 user_to_get.predicted_logout_time.minute * 60) + (
                                    user_to_get.predicted_logout_time.second)
-                    timer = end_time - present_time
-                    converted_time = timedelta(seconds=timer)
-                    return render(request, 'userapp/quiz.html',
+                        timer = end_time - present_time
+                        converted_time = timedelta(seconds=timer)
+                        return render(request, 'userapp/quiz.html',
                                   {'question': ques, 'sr': sr_no, 'score': current_score_to_show,
-                                   'remain_time': converted_time})
-            different = 0
-            while (different == 0):
-                id1 = random.randint(1, 10)
-                if str(id1) not in user_to_get.question_attempted.split(" "):
-                    different = 1
-            ques = Question.objects.get(id=id1)
-            ques_list = user_to_get.question_attempted.split(" ")
-            ques_list.append(id1)
-            user_to_get.question_attempted = " ".join(map(str, ques_list))
-            user_to_get.current_ques_id = id1
-            user_to_get.save(update_fields=["question_attempted", "current_ques_id"])
-            response = Response(question=ques, user=user)
-            response.save()
-            present_time = datetime.now()
-            present_time = (present_time.hour * 3600) + (present_time.minute * 60) + (present_time.second)
-            end_time = (user_to_get.predicted_logout_time.hour * 3600) + (
+                                   'remain_time': converted_time, 'abc': user_to_get.decision})
+                different = 0
+                while (different == 0):
+                    id1 = random.randint(1, 10)
+                    if str(id1) not in user_to_get.question_attempted.split(" "):
+                        different = 1
+                ques = Question.objects.get(id=id1)
+                ques_list = user_to_get.question_attempted.split(" ")
+                ques_list.append(id1)
+                user_to_get.question_attempted = " ".join(map(str, ques_list))
+                user_to_get.current_ques_id = id1
+                user_to_get.save(update_fields=["question_attempted", "current_ques_id"])
+                response = Response(question=ques, user=user)
+                response.save()
+                present_time = datetime.now()
+                present_time = (present_time.hour * 3600) + (present_time.minute * 60) + (present_time.second)
+                end_time = (user_to_get.predicted_logout_time.hour * 3600) + (
                         user_to_get.predicted_logout_time.minute * 60) + (user_to_get.predicted_logout_time.second)
-            timer = end_time - present_time
-            converted_time = timedelta(seconds=timer)
-            current_score_to_show = user_to_get.current_score
-            sr_no = user_to_get.no_question_solved + 1
-            return render(request, 'userapp/quiz.html', {'question': ques, 'sr': sr_no, 'score': current_score_to_show,
-                                                         'remain_time': converted_time})
-        except Question.DoesNotExist:
-            return redirect('quiz')
+                timer = end_time - present_time
+                converted_time = timedelta(seconds=timer)
+                current_score_to_show = user_to_get.current_score
+                sr_no = user_to_get.no_question_solved + 1
+                return render(request, 'userapp/quiz.html', {'question': ques, 'sr': sr_no, 'score': current_score_to_show,
+                                                         'remain_time': converted_time, 'abc': user_to_get.decision})
+            except Question.DoesNotExist:
+                return redirect('quiz')
+    except:
+        return render(request, 'userapp/SignIn.html', {'message': 'You need to login.'})
 
 
+@never_cache
 def quiz_post(request):
     username = None
     if request.user.is_authenticated:
         username = request.user.username
     user = User.objects.get(username=username)
     if request.method == 'POST':
-        
         user_to_update = UserProfile.objects.get(user=request.user)
-        
-        idk=user_to_update.decision
-        
         response_to_update = Response.objects.filter(user=user.id).last()
         present_time = datetime.now()
         present_time = (present_time.hour * 3600) + (present_time.minute * 60) + (present_time.second)
-        end_time = (user_to_update.predicted_logout_time.hour * 3600) + (
-                    user_to_update.predicted_logout_time.minute * 60) + (user_to_update.predicted_logout_time.second)
+        end_time = (user_to_update.predicted_logout_time.hour * 3600) + (user_to_update.predicted_logout_time.minute * 60) + (user_to_update.predicted_logout_time.second)
         timer = end_time - present_time
         if timer <= 0:
             return redirect('logout')
-
         if user_to_update.decision == 0:
-            present_time = datetime.now()
-            present_time = (present_time.hour * 3600) + (present_time.minute * 60) + (present_time.second)
-            end_time = (user_to_update.predicted_logout_time.hour * 3600) + (
-                        user_to_update.predicted_logout_time.minute * 60) + (
-                            user_to_update.predicted_logout_time.second)
-            timer = end_time - present_time
-            converted_time = timedelta(seconds=timer)
-            idk=user_to_update.decision
             attempt_1 = request.POST.get("attempt1")
             response_to_update.attempt_1 = attempt_1
             response_to_update.save(update_fields=["attempt_1"])
@@ -192,32 +182,29 @@ def quiz_post(request):
                 user_to_update.no_question_solved = user_to_update.no_question_solved + 1
                 user_to_update.current_score = user_to_update.current_score + 4
                 user_to_update.save(update_fields=["no_question_solved", "current_score"])
-                return render(request,'userapp/quiz.html',{'abc':idk, "remain_time": converted_time, 'question': Question.objects.get(id=user_to_update.current_ques_id),
-                               'sr': user_to_update.no_question_solved + 1, 'score': user_to_update.current_score})
+                return HttpResponseRedirect(reverse('quiz'))
             else:
-                idk = 1
+                user_to_update.decision = 1
                 present_time = datetime.now()
                 present_time = (present_time.hour * 3600) + (present_time.minute * 60) + (present_time.second)
-                end_time = (user_to_update.predicted_logout_time.hour * 3600) + (
-                            user_to_update.predicted_logout_time.minute * 60) + (
-                               user_to_update.predicted_logout_time.second)
+                end_time = (user_to_update.predicted_logout_time.hour * 3600) + (user_to_update.predicted_logout_time.minute * 60) + (
+                    user_to_update.predicted_logout_time.second)
                 timer = end_time - present_time
                 converted_time = timedelta(seconds=timer)
                 user_to_update.save(update_fields=['current_score', "decision"])
                 return render(request, 'userapp/quiz.html',
                               {'question': Question.objects.get(id=user_to_update.current_ques_id),
                                'sr': user_to_update.no_question_solved + 1, 'score': user_to_update.current_score,
-                               "remain_time": converted_time, 'abc': idk})
-        elif idk == 1:
+                               "remain_time": converted_time, 'abc' :user_to_update.decision, 'attempt1':attempt_1})
+        elif user_to_update.decision == 1:
             attempt_2 = request.POST.get("attempt2")
             response_to_update.save(update_fields=["attempt_2"])
             response_to_update.attempt_2 = attempt_2
-            idk = 0
+            user_to_update.decision = 0
             user_to_update.no_question_solved = user_to_update.no_question_solved + 1
             if int(attempt_2) == Question.objects.get(id=user_to_update.current_ques_id).correct_ans:
                 user_to_update.current_score = user_to_update.current_score + 2
             else:
                 user_to_update.current_score = user_to_update.current_score - 1
             user_to_update.save(update_fields=['current_score', 'no_question_solved', "decision"])
-            return render(request,'userapp/quiz.html',{'abc':idk, 'question': Question.objects.get(id=user_to_update.current_ques_id),
-                               'sr': user_to_update.no_question_solved + 1, 'score': user_to_update.current_score,"remain_time": converted_time})
+            return HttpResponseRedirect(reverse('quiz'))
